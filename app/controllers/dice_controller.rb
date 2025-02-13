7d9196613f9a5ba53b857104c84489d78c0d9e14
Preston Fegley <fegley.preston@gmail.com>
2025-02-13 01:42:12 +0000

    second break commit

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,7 @@
+class DiceController < ApplicationController
+
+  def diceindex
+    render :template => "dice/index"
+  end
+
+end
--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,7 +1,11 @@
 class TacosController < ApplicationController
 
   def index
-    render :inline => "<h1>How do you like them tacos?</h1>"
+    # render :inline => "<h1>How do you like them tacos?</h1>"
+      # You can do this, but it's not very elegant when you have a lot of code
+      # Better practice to add an index.html.erb file under views/tacos
+    render :template => "tacos/index"
+      # Technically don't need this, rails will interpret it as long as your "action", "def", and the views file are the same name
   end
 
 end
